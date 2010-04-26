193fb1d71af765c65455e952e6814b4f895ec7b3
Olexiy Zamkoviy <olexiy.z@gmail.com>
2010-04-26 20:32:32 +0300

    Small fixes

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -5,6 +5,7 @@ class EventTest < ActiveSupport::TestCase
   should_validate_presence_of :title, :content
   should_validate_numericality_of :cost
   should_validate_presence_of :start_time
+  should_ensure_length_in_range :title, 0..150
 
   context "Event::last_event_date" do
     should "return correct date" do
