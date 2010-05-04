1e6dbb1f192f54c26e47eaa9293b30e032594e0a
Olexiy Zamkoviy <olexiy.z@gmail.com>
2010-05-04 21:31:27 +0300

    Fixed city selector for register form

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,4 +1,4 @@
 $(function(){
   $('#typus_user_birth_date').datepicker();
-  applySelectChain($('#typus_users_root_place'), $('#typus_user_city_id'), '/events/cities');
+  applySelectChain($('#events_root_place'), $('#typus_user_city_id'), '/events/cities');
 });
