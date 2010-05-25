fafaa5f5894a16acf93825c07c532384b358fbbe
Olexiy Zamkoviy <olexiy.z@gmail.com>
2010-05-25 15:56:05 +0300

    Added strip_tags for content in news list

--- test/functional/news_controller_test.rb
+++ test/functional/news_controller_test.rb
@@ -2,7 +2,18 @@ require 'test_helper'
 
 class NewsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "index action" do
+    should "display truncated and stripped from tags description text" do
+      Post.delete_all
+      @text =  "<b>#{Faker::Lorem::sentence}</b><i>#{Faker::Lorem::sentence}</i>"
+      @post = Post.make :content => @text
+      get :index
+
+      @view = @response.template
+
+      assert_select '.section td.text_3', 1 do |t|
+        assert t.first.children.first.to_s.match @view.truncate @view.strip_tags(@text), :length => 200
+      end
+    end
   end
 end
