755e786298b64715f52ef5bf120e80073dba2827
Olexiy Zamkoviy <olexiy.z@gmail.com>
2010-09-25 14:19:57 +0300

    Fixed pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def pagination_for(items)
-    render :partial => '/pagination', :locals => { :items => items }
+    will_paginate items
   end
 
   def stylesheet(*args)
