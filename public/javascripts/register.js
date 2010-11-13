cde836edabb31e50027c64bf5c1166840df27057
Olexiy Zamkoviy <olexiy.z@gmail.com>
2010-11-13 20:42:59 +0200

    Fixed registration datepicker

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,4 +1,8 @@
 $(function(){
-  $('#typus_user_birth_date').datepicker();
+  $('#typus_user_birth_date').datepicker({
+    showOn: "button",
+    buttonImage: "images/calendar.gif",
+    buttonImageOnly: true
+  });
   applySelectChain($('#events_root_place'), $('#typus_user_city_id'), '/events/cities');
 });
